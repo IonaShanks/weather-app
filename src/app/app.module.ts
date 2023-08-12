157839131b1f38403aff971eaf2a430ecb3e154c
Iona Shanks <Ionashanks@outlook.com>
2023-08-12 13:52:50 +0200

    Add temperature gauge
    Temperature gauge added to display-card
    Using igniteui gauges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WeatherComponent } from './weather/weather.component';
 import { DisplayCardComponent } from './display-card/display-card.component';
+import { IgxRadialGaugeModule } from 'igniteui-angular-gauges';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, IgxRadialGaugeModule],
   declarations: [AppComponent, WeatherComponent, DisplayCardComponent],
   bootstrap: [AppComponent]
 })
